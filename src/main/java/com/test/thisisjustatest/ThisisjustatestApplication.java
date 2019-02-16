b01983dd706956538974bceb7c73fdf3ace3867e
Kevin Fabian <akame1v33@gmail.com>
2019-02-16 13:05:50 +0800

    'fuck'

---
+++ src/main/java/com/test/thisisjustatest/ThisisjustatestApplication.java
@@ -0,0 +1,14 @@
+package com.test.thisisjustatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisisjustatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisisjustatestApplication.class, args);
+	}
+
+}
+
